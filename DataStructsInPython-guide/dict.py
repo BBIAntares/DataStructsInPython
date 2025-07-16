70fba90e2198301bcb95eb660efd31538231576f
BBIAntares <daniiltepikin8@gmail.com>
2025-07-16 14:04:36 +0300

    add comments

--- DataStructsInPython-guide/dict.py
+++ DataStructsInPython-guide/dict.py
@@ -1,10 +1,11 @@
+# пустой словарь + элем для push'a значений
 dictionaryCharKInt = {}
 x = 0
 
-
+# push элементов в словарь (ключ : значение) Можно 2й input для значения написать
 while len(dictionaryCharKInt) < 5:
     intIt+=1
     dictionaryCharKInt[input()] = x
-    
 
+# принтим количество элементов в словаре 
 print(len(dictionaryCharKInt))
