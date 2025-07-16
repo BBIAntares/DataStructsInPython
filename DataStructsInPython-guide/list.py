0519a145aa33c76fa0afc9c3514c0760fadb4245
BBIAntares <daniiltepikin8@gmail.com>
2025-07-16 03:57:51 +0300

    add comments

--- DataStructsInPython-guide/list.py
+++ DataStructsInPython-guide/list.py
@@ -1,21 +1,23 @@
-
+# эт функция, которая принтит список 
 def printList(listArg):
     for it in listArg:
         print(it)
 
-
+# пустой список + элем для push'a 
 x = 0
 listOfNumbers = []
 
-
+# push элементов в список 
 while len(listOfNumbers) < 10:
     x+=1
     listOfNumbers.append(x)
 
 printList(listOfNumbers)  
 
-
+# фулл pop элементов
 while len(listOfNumbers) != 0:
     listOfNumbers.pop();
 
-printList(listOfNumbers) 
+printList(listOfNumbers) # принтит пустой список
+
+# для pop'a и push'a можно написать функции, но синтаксис меня душит 
