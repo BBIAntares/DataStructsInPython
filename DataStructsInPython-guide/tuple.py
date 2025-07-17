8795f7d9b1c6f48d88ad629d22cf654ceb49c447
BBIAntares <daniiltepikin8@gmail.com>
2025-07-17 03:47:05 +0300

    add comments

--- DataStructsInPython-guide/tuple.py
+++ DataStructsInPython-guide/tuple.py
@@ -1,5 +1,6 @@
+# 3 - 0, 4 - 1, 5 - 2, 8 - 3 | в программировании нумерация элементов начинается с нуля)
 tupleOfNumbers = (3, 4, 5, 8) 
 
-print(tupleOfNumbers[0])
+print(tupleOfNumbers[0]) # принтим первый элем 
 print(tupleOfNumbers[3])
 
